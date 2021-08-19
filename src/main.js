145ab6df5c9ac7d3a707dc11ed5002316d5795b6
FangFeiyue <18766131540@163.com>
2021-08-19 19:22:05 +0800

    引入ElementPlus

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index'
+import ElementPlus from 'element-plus'
+import 'element-plus/packages/theme-chalk/src/index.scss'
 
 const app = createApp(App)
-app.use(router).mount('#app')
+app.use(router).use(ElementPlus).mount('#app')
