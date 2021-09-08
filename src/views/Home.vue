c65d252bf7d53f6322f5ea3cee7626704133fd45
FangFeiyue <18766131540@163.com>
2021-09-08 17:18:37 +0800

    首页菜单功能实现

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,50 +1,178 @@
 <template>
   <div class="basic-layout">
-    <div class="nav-side"></div>    
-    <div class="content-right">
-       <div class="nav-top">
-         <div class="bread">面包屑</div>
-         <div class="user-info">用户</div>
-       </div>
-       <div class="wrapper">
-         <div class="main-page">
+    <div :class="['nav-side', isCollapse ? 'fold' : 'unfold']">
+      <div class="logo">
+        <img src="../assets/logo.png" />
+        <span v-show="!isCollapse">Manager</span>
+      </div>
+      <el-menu
+        default-active="2"
+        background-color="#001529"
+        text-color="#fff"
+        router
+        :collapse="isCollapse"
+        class="nav-menu"
+      >
+        <el-submenu index="1">
+          <template #title>
+            <i class="el-icon-location"></i>
+            <span>系统管理</span>
+          </template>
+          <el-menu-item index="1-1">用户管理</el-menu-item>
+          <el-menu-item index="1-2">菜单管理</el-menu-item>
+        </el-submenu>
+        <el-submenu index="2">
+          <template #title>
+            <i class="el-icon-location"></i>
+            <span>审批管理</span>
+          </template>
+          <el-menu-item index="2-1">休假申请</el-menu-item>
+          <el-menu-item index="2-2">待我审批</el-menu-item>
+        </el-submenu>
+      </el-menu>
+    </div>
+    <div :class="['content-right', isCollapse ? 'fold' : 'unfold']">
+      <div class="nav-top">
+        <div class="nav-left">
+          <div class="menu-fold" @click="toggle">
+            <i class="el-icon-s-fold"></i>
+          </div>
+          <div class="bread">面包屑</div>
+        </div>
+        <div class="user-info">
+          <el-badge
+            is-dot
+            class="notice"
+            type="danger"
+            @click="$router.push('/audit/approve')"
+          >
+            <i class="el-icon-bell"></i>
+          </el-badge>
+          <el-dropdown @command="handleLogout">
+            <span class="user-link">
+              {{ userInfo.userName }}
+              <i class="el-icon--right"></i>
+            </span>
+            <template #dropdown>
+              <el-dropdown-menu>
+                <el-dropdown-item command="email"
+                  >邮箱：{{ userInfo.userEmail }}</el-dropdown-item
+                >
+                <el-dropdown-item command="logout">退出</el-dropdown-item>
+              </el-dropdown-menu>
+            </template>
+          </el-dropdown>
+        </div>
+      </div>
+      <div class="wrapper">
+        <div class="main-page">
           <router-view></router-view>
-         </div>
-       </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      isCollapse: false,
+      userInfo: {
+        userName: "房飞跃",
+        userEmail: "29",
+      },
+    };
+  },
+  methods: {
+    toggle() {
+      this.isCollapse = !this.isCollapse;
+    },
+    handleLogout(key) {
+      if (key === "email") return;
+      this.$store.commit("saveUserInfo", "");
+      this.userInfo = null;
+      this.$router.replace("/login");
+    },
+  },
+};
+</script>
 <style lang="scss" scoped>
-  .basic-layout {
-    position: relative;
-    .nav-side {
-      position: fixed;
-      width: 200px;
-      height: 100vh;
-      background-color: #001529;
-      color: #fff;
-      overflow-y: auto;
-      transition: width 0.5s;
-    }
-    .content-right {
-      margin-left: 200px;
-      .nav-top {
-        height: 50px;
-        line-height: 50px;
-        display: flex;
-        justify-content: space-between;
-        border-bottom: 1px solid #ddd;
-        padding: 0 20px;
+.basic-layout {
+  position: relative;
+  .nav-side {
+    position: fixed;
+    width: 200px;
+    height: 100vh;
+    background-color: #001529;
+    color: #fff;
+    overflow-y: auto;
+    transition: width 0.5s;
+    .logo {
+      display: flex;
+      align-items: center;
+      font-size: 18px;
+      height: 50px;
+      img {
+        margin: 0 16px;
+        width: 32px;
+        height: 32px;
       }
-      .wrapper {
-        background-color: #eef0f3;
-        padding: 20px;
-        height: calc(100vh - 50px);
-        .main-page {
-          height: 100%;
-          background-color: #fff;
+    }
+    .nav-menu {
+      height: calc(100vh - 50px);
+      border-right: none;
+    }
+    &.fold {
+      width: 64px;
+    }
+    &.unfold {
+      width: 200px;
+    }
+  }
+  .content-right {
+    margin-left: 200px;
+    &.fold {
+      margin-left: 64px;
+    }
+    &.unfold {
+      margin-left: 200px;
+    }
+    .nav-top {
+      height: 50px;
+      line-height: 50px;
+      display: flex;
+      justify-content: space-between;
+      border-bottom: 1px solid #ddd;
+      padding: 0 20px;
+      .nav-left {
+        display: flex;
+        align-items: center;
+        .menu-fold {
+          margin-right: 15px;
+          font-size: 18px;
+        }
+      }
+      .user-info {
+        .notice {
+          line-height: 30px;
+          margin-right: 15px;
+          cursor: pointer;
+        }
+        .user-link {
+          cursor: pointer;
+          color: #409eff;
         }
       }
     }
+    .wrapper {
+      background-color: #eef0f3;
+      padding: 20px;
+      height: calc(100vh - 50px);
+      .main-page {
+        height: 100%;
+        background-color: #fff;
+      }
+    }
   }
+}
 </style>
