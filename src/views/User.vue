df2fa16f78b12b90b34e01fb4b01acf72fa3ea13
FangFeiyue <18766131540@163.com>
2021-09-14 11:48:02 +0800

    用户新增实现

--- src/views/User.vue
+++ src/views/User.vue
@@ -24,7 +24,7 @@
     </div>
     <div class="base-table">
       <div class="action">
-        <el-button type="primary">新增</el-button>
+        <el-button type="primary" @click="handleCreate">新增</el-button>
         <el-button type="danger" @click="handlePatchDel">批量删除</el-button>
       </div>
       <el-table :data="userList" @selection-change="handleSelectionChange">
@@ -57,6 +57,63 @@
         :page-size="pager.pageSize"
       />
     </div>
+    <el-dialog title="用户新增" v-model="showModal">
+      <!-- ref 名字和 model 的名字不能相同 -->
+      <el-form
+        ref="dialogForm"
+        :rules="rules"
+        :model="userForm"
+        label-width="100px"
+      >
+        <el-form-item label="用户名" prop="userName">
+          <el-input v-model="userForm.userName" placeholder="请输入用户名" />
+        </el-form-item>
+        <el-form-item label="邮箱" prop="email">
+          <el-input v-model="userForm.email" placeholder="请输入用户邮箱">
+            <!-- input 插槽的使用 -->
+            <template #append> @163.com </template>
+          </el-input>
+        </el-form-item>
+        <el-form-item label="手机号" prop="mobile">
+          <el-input v-model="userForm.mobile" placeholder="请输入用户手机号" />
+        </el-form-item>
+        <el-form-item label="岗位" prop="job">
+          <el-input v-model="userForm.job" placeholder="请输入岗位" />
+        </el-form-item>
+        <el-form-item label="状态" prop="state">
+          <el-select v-model="userForm.state">
+            <el-option :value="1" label="在职"></el-option>
+            <el-option :value="2" label="离职"></el-option>
+            <el-option :value="3" label="试用期"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="系统角色" prop="roleList">
+          <el-select
+            v-model="userForm.roleList"
+            placeholder="请选择用户系统角色"
+          >
+            <el-option></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="部门" prop="deptId">
+          <el-cascader
+            v-model="userForm.deptId"
+            placeholder="请选择所属部门"
+            :options="options"
+            :props="{ checkStrictly: true, value: '_id', label: 'deptName' }"
+            clearable
+          ></el-cascader>
+        </el-form-item>
+      </el-form>
+      <template #footer>
+        <span class="dialog-footer">
+          <el-button @click="dialogVisible = false">取 消</el-button>
+          <el-button type="primary" @click="dialogVisible = false"
+            >确 定</el-button
+          >
+        </span>
+      </template>
+    </el-dialog>
   </div>
 </template>
 <script>
@@ -71,10 +128,49 @@ export default {
 
     // 设置默认值
     const user = reactive({
-      state: 1,
+      state: 3,
     });
-
+    const rules = reactive({
+      userName: [
+        {
+          required: true,
+          message: "请输入用户名称",
+          trigger: "blur",
+        },
+      ],
+      email: [
+        {
+          required: true,
+          message: "请输入用户邮箱",
+          trigger: "blur",
+        },
+      ],
+      mobile: [
+        {
+          pattern: /1\d{10}/,
+          message: "请输入正确的手机号格式",
+          trigger: "blur",
+        }
+      ],
+      deptId: [
+        {
+          required: true,
+          message: "请输入用户邮箱",
+          trigger: "blur",
+        },
+      ],
+    });
+    const userForm = reactive({
+      state: 1,
+      job: "",
+      email: "",
+      mobile: "",
+      deptId: "",
+      roleList: [],
+      userName: "",
+    });
     const userList = ref([]);
+    const showModal = ref(false);
     const pager = reactive({
       total: 10,
       pageNum: 1,
@@ -100,21 +196,21 @@ export default {
         formatter(row, column, value, index) {
           console.log(row, column, value, index);
           return {
-            0: '管理员',
-            1: '普通用户'
+            0: "管理员",
+            1: "普通用户",
           }[value];
-        }
+        },
       },
       {
         label: "用户状态",
         prop: "state",
         formatter(row, column, value) {
           return {
-            1: '在职',
-            2: '离职',
-            3: '试用期'
+            1: "在职",
+            2: "离职",
+            3: "试用期",
           }[value];
-        }
+        },
       },
       {
         label: "注册时间",
@@ -169,7 +265,7 @@ export default {
         if (res.nModified > 0) {
           $message.success("删除成功");
           getUserList();
-        }else {
+        } else {
           $message.success("删除失败");
         }
       } catch (err) {
@@ -179,16 +275,24 @@ export default {
     const handleSelectionChange = (list) => {
       checkedUserIds = list.map((item) => item.userId);
     };
+    const handleCreate = () => {
+      console.log("111");
+      showModal.value = true;
+    };
     return {
       user,
+      rules,
       pager,
       columns,
       userList,
+      userForm,
+      showModal,
       checkedUserIds,
 
       handleDel,
       handleQuery,
       handleReset,
+      handleCreate,
       handlePatchDel,
       handleCurrentChange,
       handleSelectionChange,
