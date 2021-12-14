0ce1085e5001a4e08f450f387188e2aafcf116d4
FangFeiyue <18766131540@163.com>
2021-12-14 18:36:28 +0800

    审批休假申请

--- src/api/index.js
+++ src/api/index.js
@@ -134,5 +134,13 @@ export default {
 			data: params,
 			mock: false
 		});
+	},
+	leaveApprove(params) {
+		return request({
+			url: '/leave/approve',
+			method: 'post',
+			data: params,
+			mock: false
+		});
 	}
 };
--- src/views/Approve.vue
+++ src/views/Approve.vue
@@ -31,8 +31,11 @@
         <template #default="scope">
           <el-button
             size="small"
-            @click="handleClick"
-            :disabled="scope.row.curAuditUserName != userInfo.userName"
+            :disabled="
+              scope.row.curAuditUserName != userInfo.userName ||
+              scope.row.applyState > 2
+            "
+            @click="handleAudit(scope.row)"
             >审核</el-button
           >
         </template>
@@ -46,11 +49,53 @@
       @current-change="handleChangePagination"
     >
     </el-pagination>
+    <el-dialog v-model="showDialog" title="审核" width="50%">
+      <el-form
+        ref="dialogForm"
+        :model="ruleForm"
+        :rules="rules"
+        label-width="100px"
+        label-suffix="："
+      >
+        <el-form-item label="申请人">{{ applyDetail.applyUser }}</el-form-item>
+        <el-form-item label="休假类型">{{
+          applyDetail.applyTypeName
+        }}</el-form-item>
+        <el-form-item label="休假时间">{{ applyDetail.time }}</el-form-item>
+        <el-form-item label="休假时长">{{
+          applyDetail.leaveTime
+        }}</el-form-item>
+        <el-form-item label="休假原因">{{ applyDetail.reasons }}</el-form-item>
+        <el-form-item label="审批状态">{{
+          applyDetail.applyStateName
+        }}</el-form-item>
+        <el-form-item label="审批人">{{
+          applyDetail.curAuditUserName
+        }}</el-form-item>
+        <el-form-item label="备注" prop="remark">
+          <el-input
+            v-model="ruleForm.remark"
+            placeholder="请输审批入备注"
+            type="textarea"
+            rows="3"
+          ></el-input>
+        </el-form-item>
+      </el-form>
+      <template #footer>
+        <span class="dialog-footer">
+          <el-button @click="handleApprove('refuse')">拒绝</el-button>
+          <el-button type="primary" @click="handleApprove('pass')"
+            >审批通过</el-button
+          >
+        </span>
+      </template>
+    </el-dialog>
   </div>
 </template>
 <script>
 import { reactive, ref } from "@vue/reactivity";
 import { getCurrentInstance, onMounted } from "@vue/runtime-core";
+import utils from "../utils/utils";
 export default {
   setup() {
     const { appContext, ctx } = getCurrentInstance();
@@ -125,12 +170,25 @@ export default {
       pageSize: 10,
       total: 0,
     });
+    const ruleForm = reactive({
+      remark: "",
+    });
     const userInfo = $store.state.userInfo;
-
+    const rules = reactive({
+      remark: [
+        {
+          type: "string",
+          required: true,
+          message: "请输入备注",
+          trigger: "blur",
+        },
+      ],
+    });
     const approveList = ref([]);
     onMounted(() => {
       getApproveList();
     });
+    const showDialog = ref(false);
     const getApproveList = async () => {
       const params = { ...queryForm, ...pager, type: "approve" };
       const { list, page } = await $api.getApplyList(params);
@@ -147,14 +205,88 @@ export default {
       pager.pageNum = val;
       getApproveList();
     };
+    const handleClose = (form) => {
+      handleReset(form);
+      showDialog.value = false;
+    };
+    const applyDetail = ref({});
+    const handleApprove = (action) => {
+      ctx.$refs["dialogForm"].validate(async (valid) => {
+        if (valid) {
+          const params = {
+            action,
+            remark: ruleForm.remark,
+            _id: applyDetail.value._id,
+          };
+          try {
+            await $api.leaveApprove(params);
+            $message.success("操作成功");
+            handleClose("dialogForm");
+            getApproveList();
+          } catch (err) {
+            console.log(err);
+          }
+        } else {
+          console.log("提交失败");
+          return false;
+        }
+      });
+    };
+    const handleAudit = (row) => {
+      const {
+        _id,
+        reasons,
+        applyState,
+        applyType,
+        curAuditUserName,
+        leaveTime,
+        endTime,
+        startTime,
+        applyUser,
+      } = row;
+      const applyStateName = {
+        1: "待审批",
+        2: "审批中",
+        3: "审批拒绝",
+        4: "审批通过",
+        5: "作废",
+      }[applyState];
+      const applyTypeName = {
+        1: "事假",
+        2: "调休",
+        3: "年假",
+      }[applyType];
+      let time =
+        utils.formateDate(startTime, "yyyy-MM-dd") +
+        "-" +
+        utils.formateDate(endTime, "yyyy-MM-dd");
+
+      applyDetail.value = {
+        _id,
+        time,
+        applyUser: applyUser.userName,
+        reasons,
+        leaveTime,
+        applyStateName,
+        applyTypeName,
+        curAuditUserName,
+      };
+      showDialog.value = true;
+    };
     return {
       pager,
+      rules,
       columns,
+      ruleForm,
       userInfo,
       queryForm,
+      showDialog,
+      applyDetail,
       approveList,
+      handleAudit,
       handleReset,
       handleQuery,
+      handleApprove,
       handleChangePagination,
     };
   },
