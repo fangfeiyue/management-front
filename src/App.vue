25411c7845c0da63c585230b2044558453b9cbc7
FangFeiyue <18766131540@163.com>
2021-12-21 16:47:22 +0800

    v-model

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,26 @@
 <template>
-  <router-view></router-view>
+  <!-- <router-view></router-view> -->
+  <my-input
+    v-model="userName"
+    v-model:title="pageTitle"
+    @handleSubmit="handleSubmit"
+  />
 </template>
+<script>
+import MyInput from "./components/MyInput.vue";
+export default {
+  components: { MyInput },
+  data() {
+    return {
+      userName: "f",
+      pageTitle: "home",
+    };
+  },
+  methods: {
+    handleSubmit(val) {
+      console.log(val);
+      console.log(this.userName);
+    },
+  },
+};
+</script>
---
+++ src/components/MyInput.vue
@@ -0,0 +1,30 @@
+<template>
+  <el-input :modelValue="userName" @update:modelValue="handleInput" />
+  <button @click="handleSubmit">点击测试</button>
+</template>
+<script>
+import { ref } from "@vue/reactivity";
+export default {
+  name: "VInput",
+  props: {
+    title: String,
+    modelValue: String,
+  },
+  setup(props, context) {
+    const userName = ref(props.modelValue);
+    const handleInput = (val) => {
+      console.log(val);
+      userName.value = val;
+      context.emit("update:modelValue", val);
+    };
+    const handleSubmit = () => {
+      context.emit("handleSubmit", userName);
+    };
+    return {
+      userName,
+      handleInput,
+      handleSubmit,
+    };
+  },
+};
+</script>
